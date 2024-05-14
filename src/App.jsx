45d2ada212dfb15b20b0e193ba8c8a18dd831198
Ganiyat Lateef-Okunade <ganiyat1425@gmail.com>
2024-05-14 17:36:59 +1000

    New update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
 import Button from "./components/Button";
+import "./app.css";
+import { useState } from "react";
+import FormPanel from "./components/FormPanel";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const send = (msg) => console.log(msg);
+
+  const increment = () => {
+    if (count < 10) {
+      setCount(count + 1);
+    }
+  };
+
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
   return (
     <>
-      <div>App</div>
-      <Button label="Button1" color="skyBlue" />
-      <Button label="Button2" color="lightPink" />
-      <Button label="Button3" />
-      <Button color="skyBlue" />
-      <Button />
+      <div>My Form</div>
+      <div className="panel"> {count} </div>
+      <Button label="-" action={decrement} />
+      <Button label="+" action={increment} />
+      <FormPanel count={count} />
+      <Button label="Button1" color="blue" action={() => send("Button1")} />
+      <Button color="skyBlue" action={() => send("Button2")} />
     </>
   );
 };
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,13 @@
-const Button = ({label= "Mybutton", color= "lightyellow"}) => {
-    const style = {
-        backgroundColor: color
-    }
-    return (<button style={style}> {label}</button>);
-}
+const Button = (props) => {
+  const { action, label = "My Button", color } = props;
+  const style = {
+     backgroundColour: color,
+     };
+  return (
+    <button className="btn" style={style} onClick={action}>
+      {label}
+    </button>
+  );
+};
 
 export default Button;
